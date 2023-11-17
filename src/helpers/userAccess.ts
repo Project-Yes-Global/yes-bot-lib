03e35085863659bfd1541d983895333d84ca0758
Ivan Bunin <chernobelenkiy@gmail.com>
2023-11-18 01:52:54 +0300

    add callback

--- src/helpers/userAccess.ts
+++ src/helpers/userAccess.ts
@@ -1,15 +1,19 @@
 import TelegramBot from 'node-telegram-bot-api';
+import { extractHashtags } from './hashTags';
+import { YesBotConfig } from '../types';
 
 export type TUserAccess = {
   canReply: boolean;
 }
 
-export const createUserAccess = (msg: TelegramBot.Message): TUserAccess => {
+export const createUserAccess = (config: YesBotConfig, msg: TelegramBot.Message): TUserAccess => {
   const { chat: { type: chatType }, from } = msg;
   const userId = from?.id?.toString();
   const canReply = chatType === 'private' && !!userId;
+  const hashTags = extractHashtags(msg?.text || '');
 
   return {
-    canReply
+    canReply,
+    ...(config.callbacks.onUserAccess && config.callbacks.onUserAccess(hashTags, msg)),
   }
 }
